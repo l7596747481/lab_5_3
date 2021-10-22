eb631b34a985227516c05f44fe93bfe23023b990
bwaah1 <chykivan@gmail.com>
2021-10-22 12:38:34 +0300

    Add project files.

---
+++ 5_3.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+double p(const double x);
+int main()
+{
+    double qp, qk, z;
+    int n;
+    cout << "qp = "; cin >> qp;
+    cout << "qk = "; cin >> qk;
+    cout << "n = "; cin >> n;
+    double dq = (qk - qp) / n;
+    double q = qp;
+    while (q <= qk)
+    {
+        z = p(2 * q + 1) + pow(p(q * q - 1), 2) + sqrt(p(1));
+        cout << q << "              " << z << endl;
+        q += dq;
+    }
+    return 0;
+}
+double p(const double x)
+{
+    if (abs(x) >= 1)
+        return (sin(x) + cos(x)) / (pow(sin(x), 2) + exp(x));
+    else
+    {
+        double S = 0;
+        int j = 0;
+        double a = 0.5;
+        S = a;
+        do
+        {
+            j++;
+            double R = - (pow(x, 2) / (4 * pow(j, 2) - 2 * j));
+            a *= R;
+            S += (1. / cos(x)) * a;
+        } while (j < 4);
+        return S;
+    }
+}
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,22 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int R;
+			double x = 5;
+			double j = 2;
+
+			R = -(pow(x, 2) / (4 * pow(j, 2) - 2 * j));
+			Assert::AreEqual(R, - 25 / 12);
+		}
+	};
+}
